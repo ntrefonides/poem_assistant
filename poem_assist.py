2ebd14c190aeb4cad80858674b93f50d9634c1b3
ntrefonides <ntrefonides@gmail.com>
2018-01-15 16:21:43 -0500

    Add poem_assist.py

---
+++ poem_assist.py
@@ -0,0 +1,4 @@
+list = ["kumquat", "nipple", "leotard", "California enzyme","plug drop", "jerrymandering", "escape drum","juggling catball","end of flameness cave"]
+listy = ["      ", "                                              "]
+import random
+print (random.choice(list), end=''); print (random.choice(list), end=''); print (random.choice(listy), end=''); print (random.choice(list), end='')
