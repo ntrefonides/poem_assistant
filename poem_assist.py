ff31048b1ea691b71f882621e0d1eb8172f092b0
ntrefonides <ntrefonides@gmail.com>
2018-01-21 15:12:31 -0500

    added new list to poem_assist.py

--- poem_assist.py
+++ poem_assist.py
@@ -1,4 +1,17 @@
-list = ["kumquat", "nipple", "leotard", "California enzyme","plug drop", "jerrymandering", "escape drum","juggling catball","end of flameness cave"]
-listy = ["      ", "                                              "]
+
+
+#chooses a random fragment of language and a random distance between fragment. 103 iterations
+
+
+list = ["kumquat", "nipple", "leotard", "California enzyme","plug drop", "jerrymandering", "escape drum","juggling catball","end of flameness cave", "penis", "bottle opener", "super", "seeds", "honey", "naked", "tangerine dream", "lavender", "tongue", "harpoon piercing", "plastic", "light trail", "orange rind", "mud", "dog", "good dog", "the entrance", "endless dust cloud", "escaping", "black", "thin sleep", "individual-real", "facile", "concessions", "obscure from herself", "suffused", "retreat", "shh", "exposition", "rhodomontade", "disintegrating man", "tyranny of analogy", "curiosity"] 
+
+
+listx = ["pressing mountains into craters", "toilet paper tail", "grating cheese snow", "amber aeroplane", "electrified body", "crater", "melting bodies hard but rocked", "3D printed", "collosal squid", "implied", "vanity", "deformity"]
+
+
+
+
+listy = ["      ", "                     ", "  ", "      ", "  ","                                        "]
 import random
-print (random.choice(list), end=''); print (random.choice(list), end=''); print (random.choice(listy), end=''); print (random.choice(list), end='')
+for x in range(0, 103):
+  print (random.choice(list), end=''); print (random.choice(listy), end=''); print (random.choice(list), end=''); print (random.choice(listy), end='');
