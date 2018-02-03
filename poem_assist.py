e009e368c4711cb457070bb1190f44b3dc1dd322
ntrefonides <ntrefonides@gmail.com>
2018-02-03 00:10:11 -0500

    added all files

--- poem_assist.py
+++ poem_assist.py
@@ -6,12 +6,33 @@
 list = ["kumquat", "nipple", "leotard", "California enzyme","plug drop", "jerrymandering", "escape drum","juggling catball","end of flameness cave", "penis", "bottle opener", "super", "seeds", "honey", "naked", "tangerine dream", "lavender", "tongue", "harpoon piercing", "plastic", "light trail", "orange rind", "mud", "dog", "good dog", "the entrance", "endless dust cloud", "escaping", "black", "thin sleep", "individual-real", "facile", "concessions", "obscure from herself", "suffused", "retreat", "shh", "exposition", "rhodomontade", "disintegrating man", "tyranny of analogy", "curiosity"] 
 
 
-listx = ["pressing mountains into craters", "toilet paper tail", "grating cheese snow", "amber aeroplane", "electrified body", "crater", "melting bodies hard but rocked", "3D printed", "collosal squid", "implied", "vanity", "deformity"]
+listx = ["pressing mountains into craters", "toilet paper tail", "grating cheese snow", "amber aeroplane", "electrified body", "crater", "melting bodies hard but rocked", "3D printed", "collosal squid", "implied", "vanity", "deformity", "gah"]
 
 
 
 
-listy = ["      ", "                     ", "  ", "      ", "  ","                                        "]
+listyii = ["      ", "                     ", "  ", "      ", "  ","                                        "]
 import random
-for x in range(0, 103):
-  print (random.choice(list), end=''); print (random.choice(listy), end=''); print (random.choice(list), end=''); print (random.choice(listy), end='');
+#for x in range(0, 103):
+ # print (random.choice(listx), end=''); print (random.choice(listy), end=''); print (random.choice(listx), end=''); print (random.choice(listy), end='');
+
+
+
+sheepsleep = ["sheep","sleep","SHEEP","SLEEP"]
+
+for x in range(0,100):
+  i = random.randint(1,100)
+  print (random.choice(sheepsleep), end = "")
+  for x in range(0,i):
+     print (random.randint(1,101), end=""); 
+
+
+#iWISHmyselfAGainstHEwind = ["
+
+
+
+
+
+
+
+
