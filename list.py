87a90d03dd5df084ce9faa131bb621965983e0c5
ntrefonides <ntrefonides@gmail.com>
2018-02-19 23:04:41 -0500

    added list.py

---
+++ list.py
@@ -0,0 +1,12 @@
+#this wil create a lits of things that an abstraction is not with a final line indicating what the abstraction is
+import random
+
+abstraction = ["hate","love"]
+negative_list = ["a competitive comparison","transient and meaningless","lying in bed with somebody else","lies","shrapnel words forgotten in the stomach","drying tears in sunlight","giving up","holding me (pulling me in)"]
+positive_list = ["the wink in pitch black","a forest under frozen lake","crossing arms and locking soul","peeling off her borrowed skin","bending branches and wearing stolen flowers in the shade with our","naked in a bed of flowers"]
+
+abstract_word = random.choice(abstraction)
+
+negative_list_order = random.shuffle(negative_list)
+
+
